77d42cd5e32ca6f404e9309c3ff11aecf0c93f41
Yujin0827 <yjjeon980827@gmail.com>
2021-05-27 18:22:09 +0900

    first commit

--- app/src/main/java/com/cookandroid/scholarshiplike/MainActivity.kt
+++ app/src/main/java/com/cookandroid/scholarshiplike/MainActivity.kt
@@ -13,8 +13,6 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 open class MainActivity : AppCompatActivity(),
     BottomNavigationView.OnNavigationItemSelectedListener  {
 
-    private val hometab = HomeFragment()
-
     lateinit var tabNav : BottomNavigationView
 
     // onBackPressed 메소드 변수
