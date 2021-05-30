77366dadf0de9ab5206f92be2f869cf989a7f739
Yujin0827 <yjjeon980827@gmail.com>
2021-05-30 15:12:40 +0900

    검색창 - 수정필요 검색창 매거진 탭 수정

--- app/src/main/java/com/cookandroid/scholarshiplike/HomeSearchMagazineFragment.kt
+++ app/src/main/java/com/cookandroid/scholarshiplike/HomeSearchMagazineFragment.kt
@@ -17,9 +17,12 @@ class HomeSearchMagazineFragment : Fragment() {
     private lateinit var listAdapter: MagazineRecyclerViewAdapter
     private var db = Firebase.firestore
     var dataList: ArrayList<Post> = arrayListOf()
+    private lateinit var mContext : Context
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
+        mContext = context
+
         val sRef = db.collection("장학금")
             .document("교내").collection("강원")
             .document("강원대").collection("학과")
@@ -47,11 +50,12 @@ class HomeSearchMagazineFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         // Fragment에서 전달받은 list를 넘기면서 ListAdapter 생성
-        listAdapter = MagazineRecyclerViewAdapter(dataList)
+        listAdapter = MagazineRecyclerViewAdapter(dataList, mContext)
         listView.layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
+
         // RecyclerView.adapter에 지정
         listView.adapter = listAdapter
-
     }
 }
