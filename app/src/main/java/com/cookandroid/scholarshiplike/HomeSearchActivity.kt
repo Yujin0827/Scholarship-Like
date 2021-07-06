374e39e793e2ba4b1c227ad8a70beb1885ee2f09
Yujin0827 <yjjeon980827@gmail.com>
2021-07-06 17:57:53 +0900

    검색창하는 중

--- app/src/main/java/com/cookandroid/scholarshiplike/HomeSearchActivity.kt
+++ app/src/main/java/com/cookandroid/scholarshiplike/HomeSearchActivity.kt
@@ -1,95 +1,86 @@
 package com.cookandroid.scholarshiplike
 
 import android.app.SearchManager
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
+import android.view.KeyEvent
+import android.view.KeyEvent.KEYCODE_ENTER
 import android.view.MotionEvent
 import android.view.View
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.tabs.TabLayout
-import com.google.android.material.tabs.TabLayoutMediator
 import kotlinx.android.synthetic.main.activity_home_search.*
 
 
 class HomeSearchActivity : AppCompatActivity() {
 
-    lateinit var searchBar: EditText            // 검색창
-    lateinit var searchGoBtn : ImageView        // 찾기 버튼
-    lateinit var resultList : RecyclerView      // 검색 결과
-    lateinit var search_tabLayout : TabLayout   // 검색 결과 탭 - 장학금, 매거진
+    lateinit var searchField: EditText            // 검색창
+    lateinit var searchBtn : ImageView        // 찾기 버튼
+    lateinit var search_result_field_s : LinearLayout   // 검색 결과필드 - 장학금
+    lateinit var search_result_field_m : LinearLayout   // 검색 결과필드 - 매거진
+    lateinit var search_result_s : RecyclerView         // 검색 결과 - 장학금
+    lateinit var search_result_m : RecyclerView         // 검색 결과 - 매거진
 
     internal var textlength = 0     // EditText 글자 수
-
-    // 검색결과 탭
-    private var tabLayoutTextArray: ArrayList<String> = arrayListOf("장학금", "매거진")
-    lateinit var viewAdapter: ViewPageAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home_search)
+        Log.d("tag", "onCreate: started")
 
-        searchBar = findViewById<EditText>(R.id.searchBar)                  // 검색창
-        searchGoBtn = findViewById<ImageView>(R.id.searchGoBtn)             // 찾기 버튼
-        search_tabLayout = findViewById<TabLayout>(R.id.search_tabLayout)   // 검색 결과 탭
-//        resultList = findViewById<RecyclerView>(R.id.toolbar)             // 검색 결과
 
-        search_tabLayout.visibility = View.INVISIBLE
+        searchField = findViewById<EditText>(R.id.searchField)          // 검색창
+        searchBtn = findViewById<ImageView>(R.id.searchBtn)     // 찾기 버튼
+        search_result_field_s = findViewById<LinearLayout>(R.id.search_result_field_s)      // 검색 결과필드 - 장학금
+        search_result_field_m = findViewById<LinearLayout>(R.id.search_result_field_m)      // 검색 결과필드 - 매거진
+        search_result_s = findViewById<RecyclerView>(R.id.search_result_s)      // 검색 결과 - 장학금
+        search_result_m = findViewById<RecyclerView>(R.id.search_result_m)      // 검색 결과 - 매거진
 
-        // 어댑터 생성, 연결
-        viewAdapter = ViewPageAdapter(this)
-        viewAdapter.addFragment(HomeSearchScholarshipFragment())
-        viewAdapter.addFragment(HomeSearchMagazineFragment())
-        search_viewpager.adapter = viewAdapter
 
-        // 탭 레이아웃 이름 연결
-        TabLayoutMediator(search_tabLayout, search_viewpager){ tab, position->
-            tab.text = tabLayoutTextArray[position]
-        }.attach()
+        search_result_field_s.visibility = View.INVISIBLE
+        search_result_field_m.visibility = View.INVISIBLE
 
-        // 탭 생성
-        searchBar.setOnTouchListener(object : View.OnTouchListener {
-            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
-                when(event?.action) {
-                    MotionEvent.ACTION_DOWN -> search_tabLayout.visibility = View.VISIBLE
-                }
-
-                return v?.onTouchEvent(event) ?: true
-            }
-        })
-
-        // 검색창 입력 감지
-        searchBar.addTextChangedListener(object: TextWatcher{
-            override fun afterTextChanged(edit: Editable?) {
-
-            }
-
-            override fun beforeTextChanged(charSequence: CharSequence?, start: Int, count: Int, after: Int) {
-
-            }
-
-            override fun onTextChanged(charSequence: CharSequence?, start: Int, before: Int, count: Int) {
-                textlength = searchBar.text.length
-            }
-        })
-
-        // 쿼리 수신
-        if (Intent.ACTION_SEARCH == intent.action) {
-            intent.getStringExtra(SearchManager.QUERY)?.also { query ->
-                doMySearch(query)
-            }
+        // 검색 버튼 클릭 (돋보기)
+        searchBtn.setOnClickListener {
+            click()
         }
-        
+
+        // 검색 버튼 클릭 (키보드)
+        searchField.setOnKeyListener { v, keyCode, event ->
+            if(event.getAction() == KeyEvent.ACTION_DOWN && keyCode == KEYCODE_ENTER) {
+                click()
+            }
+            true
+        }
+
 
     }
 
-    private fun doMySearch(query: String) {
+    // 검색 버튼 클릭했을 때 동작
+    private fun click() {
+        search_result_field_s.visibility = View.VISIBLE
+        search_result_field_m.visibility = View.VISIBLE
 
+        hideKeyboard()
     }
 
+    // 키보드 제어
+    private fun hideKeyboard() {
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(searchField.windowToken, 0)
+    }
+
+    private fun firebaseSearch() {
+
+    }
 
 }
