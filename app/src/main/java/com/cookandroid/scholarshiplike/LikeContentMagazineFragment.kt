5ad7b736da892891b410db2481ef148c542f11ec
mhseo10 <mhseo1010@naver.com>
2021-07-16 15:31:06 +0900

    service

--- app/src/main/java/com/cookandroid/scholarshiplike/LikeContentMagazineFragment.kt
+++ app/src/main/java/com/cookandroid/scholarshiplike/LikeContentMagazineFragment.kt
@@ -1,17 +1,18 @@
 package com.cookandroid.scholarshiplike
+import android.content.ComponentName
 import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.Handler
+import android.os.IBinder
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import com.cookandroid.scholarshiplike.adapter.MagazineRecyclerViewAdapter
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.firestore.ktx.toObject
-import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.fragment_recycler.*
 
 
@@ -20,48 +21,42 @@ class LikeContentMagazineFragment : Fragment() {
     private val TAG = javaClass.simpleName
 
     private lateinit var listAdapter: MagazineRecyclerViewAdapter
-    private var db = Firebase.firestore
-    private var magazineList: ArrayList<String> = arrayListOf()
-    private var magazine: ArrayList<Post> = arrayListOf()
     private lateinit var mContext : Context
-    private val user = Firebase.auth.currentUser
+    private lateinit var magazine: ArrayList<Post>
+
+    // 서비스 연결
+    private lateinit var mService: DataService
+    private var mBound: Boolean = false
+
+    /** Defines callbacks for service binding, passed to bindService()  */
+    private val connection = object : ServiceConnection {
+
+        override fun onServiceConnected(className: ComponentName, service: IBinder) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            val binder = service as DataService.DataLocalBinder
+            mService = binder.getService()
+            Log.w(TAG, "Service connected")
+            mBound = true
+        }
+
+        override fun onServiceDisconnected(arg0: ComponentName) {
+            Log.w(TAG, "Service disconnected")
+            mBound = false
+        }
+    }
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
         mContext = context
+    }
 
-        val ref = db.collection("Users")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-        Log.w(TAG, "Load Firestore")
-        if (user != null) {
-            ref.document(user.uid).get().
-            addOnSuccessListener { document ->
-                if (document.data != null){
-                    if (document.data!!["likeContent"] != null) {
-                        val data = document.data!!["likeContent"] as Map<*, *>
-                        @Suppress("UNCHECKED_CAST")
-                        magazineList = data["magazine"] as ArrayList<String>
-                    }
-
-                    Log.w(TAG, magazineList.toString())
-
-                    db.collection("매거진").whereIn("title", magazineList).get()
-                        .addOnSuccessListener { result ->
-                            for (document in result) {
-                                Log.w(TAG, document.id)
-                                val data = document.toObject<Post>()
-                                Log.w(TAG, data.toString())
-                                magazine.add(data)
-                            }
-                            listAdapter.submitList(magazine)
-                        }.addOnFailureListener { exception ->
-                        Log.w(TAG, "Error getting documents: $exception")
-                    }
-                }
-            }.addOnFailureListener { exception ->
-                // 실패할 경우
-                Log.w(TAG, "Error getting documents: $exception")
-            }
+        // Bind to LocalService
+        Intent(mContext, DataService::class.java).also { intent ->
+            mContext.bindService(intent, connection, Context.BIND_AUTO_CREATE)
+            Log.w(TAG, "Service active")
         }
     }
 
@@ -71,10 +66,25 @@ class LikeContentMagazineFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // Fragment 에서 전달받은 list 를 넘기면서 ListAdapter 생성
-        listAdapter = MagazineRecyclerViewAdapter(magazine, mContext)
 
-        listView.layoutManager = GridLayoutManager(activity, 2) //그리드 레아이웃 지정
-        listView.adapter = listAdapter //어댑터 연결
+        // 2초 딜레이 후 코드 시작
+        Handler().postDelayed({
+            // 서비스의 데이터와 연결
+            magazine = mService.magazine
+
+            // Fragment 에서 전달받은 list 를 넘기면서 ListAdapter 생성
+            listAdapter = MagazineRecyclerViewAdapter(magazine, mContext)
+            listView.layoutManager = GridLayoutManager(activity, 2) //그리드 레아이웃 지정
+            listView.adapter = listAdapter //어댑터 연결
+        }, 2000) // <-- ms로 조절
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.w(TAG, "destroy")
+
+        mContext.unbindService(connection)
+        Log.w(TAG, "Service deactivate")
+        mBound = false
     }
 }
