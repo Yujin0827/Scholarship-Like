9413423c73a63084648334f7c31783cda956ad41
Yujin0827 <yjjeon980827@gmail.com>
2021-07-20 13:48:26 +0900

    scholarship ic 추가

--- app/src/main/java/com/cookandroid/scholarshiplike/HomeFragment.kt
+++ app/src/main/java/com/cookandroid/scholarshiplike/HomeFragment.kt
@@ -88,7 +88,6 @@ class HomeFragment : Fragment() {
 
         // 교내 웹사이트로 이동
         binding.univWeb.setOnClickListener {
-
             val uri = Uri.parse("http://www.kosaf.go.kr")
             val intent = Intent(Intent.ACTION_VIEW, uri)
             startActivity(intent)
