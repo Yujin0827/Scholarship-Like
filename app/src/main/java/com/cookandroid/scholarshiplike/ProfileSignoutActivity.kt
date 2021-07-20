e784367ade4e8c65c89ff6861e02396d80df71ca
kyunghee <kyunghee1210@naver.com>
2021-07-20 17:06:30 +0900

    탈퇴하기 - '비밀번호 확인 & 탈퇴 이유 입력' 추가

--- app/src/main/java/com/cookandroid/scholarshiplike/ProfileSignoutActivity.kt
+++ app/src/main/java/com/cookandroid/scholarshiplike/ProfileSignoutActivity.kt
@@ -1,12 +1,18 @@
 package com.cookandroid.scholarshiplike
 
+import android.content.Context
 import android.content.Intent
+import android.opengl.Visibility
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.cookandroid.scholarshiplike.databinding.ActivityProfileSignoutBinding
+import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
@@ -36,13 +42,23 @@ class ProfileSignoutActivity : AppCompatActivity() {
         // '탈퇴하기' 클릭 리스너
         binding.btnSignout.setOnClickListener {
             if (confirmCheckbox()) {    // 이유를 체크했을 경우
-                deleteUserDB() // 유저 DB 삭제
+                userReauth() // 사용자 재인증
             }
         }
 
         // 뒤로가기 클릭 리스너
         binding.btnSignoutBack.setOnClickListener {
             finish()
+        }
+
+        // 기타 클릭 리스너
+        binding.cbRsnEtc.setOnClickListener {
+            if (binding.cbRsnEtc.isChecked) {
+                binding.edittxtSignoutEtcAdd.visibility = View.VISIBLE
+            }
+            else {
+                binding.edittxtSignoutEtcAdd.visibility = View.GONE
+            }
         }
     }
 
@@ -55,17 +71,69 @@ class ProfileSignoutActivity : AppCompatActivity() {
                 || binding.cbRsnBetter.isChecked
                 || binding.cbRsnUseless.isChecked
                 || binding.cbRsnEtc.isChecked
+        if (binding.cbRsnEtc.isChecked && binding.edittxtSignoutEtcAdd.text.toString() == "") {
+            isChecked = false
+            Toast.makeText(this, "탈퇴 이유를 작성 해 주세요", Toast.LENGTH_SHORT).show()
 
-        if (!isChecked) {
+            binding.edittxtSignoutEtcAdd.requestFocus() // 기타 이유 입력 포커스 주기
+            // 키보드 보이기
+            val imm: InputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.toggleSoftInput(
+                InputMethodManager.SHOW_FORCED,
+                InputMethodManager.HIDE_IMPLICIT_ONLY
+            )
+        }
+        else if (!isChecked) {
             Toast.makeText(this, "탈퇴 이유를 선택해주세요", Toast.LENGTH_SHORT).show()
         }
 
         return isChecked
     }
 
-    // 현재 사용자 DB 삭제
-    private fun deleteUserDB() {
+    // 사용자 재인증
+    private fun userReauth() {
         val user = auth.currentUser
+
+        if (user != null) {
+            val userEmail: String? = user?.email
+            val userPW = binding.edittxtSignoutPw.toString()
+
+            if (userEmail != null && userPW != null) {
+                val credential = EmailAuthProvider
+                    .getCredential(userEmail!!, userPW)
+
+                user?.reauthenticate(credential)    // 사용자 재인증
+                    ?.addOnSuccessListener {
+                        Log.d(TAG, "User re-authenticated.")
+                        deleteUserDB(user)  // 유저 DB 삭제
+                    }
+                    ?.addOnFailureListener { it ->
+                        Log.d(TAG, "Fail to user re-authenticate")
+                        Toast.makeText(this, "비밀번호가 일치하지 않습니다", Toast.LENGTH_SHORT).show()
+                        binding.edittxtSignoutPw.requestFocus() // 비밀번호 입력 포커스 주기
+                        // 키보드 보이기
+                        val imm: InputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                        imm.toggleSoftInput(
+                            InputMethodManager.SHOW_FORCED,
+                            InputMethodManager.HIDE_IMPLICIT_ONLY
+                        )
+                    }
+            }
+            else if (userPW == null) {
+                Toast.makeText(this, "비밀번호를 입력하세요", Toast.LENGTH_SHORT).show()
+                binding.edittxtSignoutPw.requestFocus() // 비밀번호 입력 포커스 주기
+                // 키보드 보이기
+                val imm: InputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+                imm.toggleSoftInput(
+                    InputMethodManager.SHOW_FORCED,
+                    InputMethodManager.HIDE_IMPLICIT_ONLY
+                )
+            }
+        }
+    }
+
+    // 현재 사용자 DB 삭제
+    private fun deleteUserDB(user: FirebaseUser) {
         db.collection("Users").document(user!!.uid)
             .delete()
             .addOnSuccessListener {
